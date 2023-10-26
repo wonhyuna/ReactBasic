c8c80875efefbd304620a1a0487bb3b5f698a65b
wonhyuna <hyunawon417@gmail.com>
2023-10-27 01:31:23 +0900

    React API 호출하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import Lifecycle from './Lifecycle';
 
+//https://jsonplaceholder.typicode.com/comments 
 
 function App(){
   const [data, setData] = useState([]); //일기 데이터 빈 배열로 시작
 
   const dataId = useRef(0)
+
+  const getData = async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments'
+    ).then((res)=>res.json());
+    
+    const initData = res.slice(0, 20).map((it)=>{
+      return{
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    })
+
+    setData(initData);
+    
+  };
+
+  useEffect(()=>{
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) =>{
     const created_date = new Date().getTime();
