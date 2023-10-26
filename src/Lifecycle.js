9ceb5dafecff641db64cb9a09dd940437d6070ac
wonhyuna <hyunawon417@gmail.com>
2023-10-27 01:18:13 +0900

    React Lifecycle 제어하기 - useEffect (mount, unmount)

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,39 +1,27 @@
 import React, {useEffect, useState} from 'react';
 
-const Lifecycle = () =>{
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  //dependecy가 변하면 콜백함수 호출됨
+const UnmountTest = () => {
   useEffect(()=>{
     console.log("Mount!");
+    return () => {
+      //Unmount 시점에 실행되게 됨
+      console.log("Unmount!");
+    };
   }, []);
 
-  useEffect(()=>{
-    console.log("Update!");
-  });
-
-  useEffect(()=>{
-    console.log(`count is update : ${count}`);
-    if(count>5){
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
-      setCount(1);
-    }
-  },[count]);
-
-  useEffect(()=>{
-    console.log(`text is update : ${text}`);
-  }, [text])
-
-  return <div style = {{padding: 20}}>
-    <div>
-      {count}
-      <button onClick = {() => setCount(count+1)}> + </button>
-    </div>
-    <div>
-      <input value = {text} onChange = {(e) => setText(e.target.value)} />
-    </div>
-  </div>
+  return <div>Unmount Testing Component</div>
 }
+
+const Lifecycle = () =>{
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
+
+  return (
+    <div style = {{padding: 20}}>
+      <button onClick = {toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest/>} 
+    </div> //isVisible이 True일 때 unmounttest가 보이게 됨
+  );
+};
 
 export default Lifecycle;
